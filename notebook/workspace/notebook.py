54e72e2fc818217f5738e308f53f889fe6dc7b72
Ashwin P <aprashanth@phdata.io>
2020-11-20 13:00:14 +0530

    changed the folder structure'

---
+++ notebook/workspace/notebook.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+print("hello")
+print("new line")
+
+# COMMAND ----------
+
+print("this is a test")
+
+# COMMAND ----------
+
+#test line
+
+# COMMAND ----------
+
+#new line
